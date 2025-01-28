948f48e4e3bbe8c8c047128771109766efc3a222
Seong57 <123578952+Seong57@users.noreply.github.com>
2025-01-28 20:00:12 +0900

    [Bronze I] Title: 단어 공부, Time: 252 ms, Memory: 22212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,36 +1,35 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int[] list = new int[26];
-
-        String str = br.readLine().trim().toUpperCase();
-
-        for (int i = 0; i < str.length(); i++) {
-            if('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {}
-            list[str.charAt(i) - 'A']++;
+        String s = br.readLine();
+        
+        int[] counts = new int[26]; // 알파벳 개수 저장 배열
+        
+        // 각 문자 카운트
+        for (char c : s.toCharArray()) {
+            char upperChar = Character.toUpperCase(c);
+            counts[upperChar - 'A']++;
         }
-
-        int max = -1;
-        char ch = '?';
-
-        for(int i = 0; i < 26; i++) {
-            if(list[i] > max){
-                max = list[i];
-                ch = (char) (i + 65);
-            }else if (list[i] == max) {
-                ch = '?';
+        
+        int maxCount = -1;
+        char maxChar = '?';
+        int duplicate = 0;
+        
+        // 최대값 및 중복 체크
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > maxCount) {
+                maxCount = counts[i];
+                maxChar = (char) ('A' + i);
+                duplicate = 1; // 새로운 최대값 발견 시 중복 초기화
+            } else if (counts[i] == maxCount) {
+                duplicate++; // 동일한 최대값 발견 시 중복 증가
             }
         }
-
-        bw.write(ch);
-
-        br.close();
-        bw.close();
+        
+        System.out.println(duplicate > 1 ? '?' : maxChar);
     }
 }
