07628a835603830c8848700be7bd02f0a1eca94e
Seong57 <123578952+Seong57@users.noreply.github.com>
2025-02-21 17:48:36 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 116 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -7,82 +7,46 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String[][] arr = new String[50][50];
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        String[] arr = new String[n];
 
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = String.valueOf(s.charAt(j));
-            }
+            arr[i] = br.readLine();
         }
+        int answer = 64;
+        for (int i = 0; i < n - 7; i++) {
+            for (int j = 0; j < m - 7; j++) {
+                int startW = 0;
+                int startB = 0;
+                for (int row = i; row < 8 + i; row++) {
+                    for (int col = j; col < 8 + j; col++) {
+                        char target = arr[row].charAt(col);
 
-        int answer = Integer.MAX_VALUE;
-
-        for (int row = 0; row < n - 7; row++ ){
-            for (int col = 0; col < m - 7; col++) {
-                int cnt = 0;
-                String start = "W";
-                for (int i = row; i < 8 + row; i++) {
-                    for (int j = col; j < 8 + col; j++) {
-                        String target = arr[i][j];
-                        if (i % 2 == 0) {
-                            if (j % 2 == 0) {
-                                if (!start.equals(target)) {
-                                    cnt++;
-                                }
-                            } else {
-                                if (start.equals(target)) {
-                                    cnt++;
-                                }
+                        if (row % 2 == 0){
+                            if (col % 2== 0) {
+                                if (target == 'B') startW++;
+                                if (target == 'W') startB++;
+                            }
+                            if (col % 2 != 0) {
+                                if (target == 'W') startW++;
+                                if (target == 'B') startB++;
                             }
                         } else {
-                            if (j % 2 == 0) {
-                                if (start.equals(target)) {
-                                    cnt++;
-                                }
-                            } else {
-                                if (!start.equals(target)) {
-                                    cnt++;
-                                }
+                            if (col % 2 == 0) {
+                                if (target == 'W') startW++;
+                                if (target == 'B') startB++;
+                            }
+                            if (col % 2 != 0) {
+                                if (target == 'B') startW++;
+                                if (target == 'W') startB++;
                             }
                         }
                     }
                 }
-                if (cnt < answer) {
-                    answer = cnt;
-                }
-                start = "B";
-                cnt = 0;
-                for (int i = row; i < 8 + row; i++) {
-                    for (int j = col; j < 8 + col; j++) {
-                        String target = arr[i][j];
-                        if (i % 2 == 0) {
-                            if (j % 2 == 0) {
-                                if (!start.equals(target)) {
-                                    cnt++;
-                                }
-                            } else {
-                                if (start.equals(target)) {
-                                    cnt++;
-                                }
-                            }
-                        } else {
-                            if (j % 2 == 0) {
-                                if (start.equals(target)) {
-                                    cnt++;
-                                }
-                            } else {
-                                if (!start.equals(target)) {
-                                    cnt++;
-                                }
-                            }
-                        }
-                    }
-                }
-                if (cnt < answer) {
-                    answer = cnt;
+                int min = Math.min(startW, startB);
+                if (min < answer){
+                    answer = min;
                 }
             }
         }
