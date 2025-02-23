103ae89dd3321f2884996bd6d2fd7cb63b12a560
Seong57 <123578952+Seong57@users.noreply.github.com>
2025-02-23 16:35:55 +0900

    [Silver V] Title: 나이순 정렬, Time: 1596 ms, Memory: 63880 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -16,6 +16,10 @@ public class Main {
             matrix[i][1] = st.nextToken();
         }
 
+        // 객체 배열은 안정 정렬(Stable Sort)을 보장한다.
+        // 안정 정렬이란 동일한 키값을 가진 요소들의 원본 순서를 유지하는 것
+        //  1차 기준: 나이 비교 (s1[0] - s2[0])
+        //  2차 기준: 원본 배열의 입력 순서 (코드에 명시되지 않지만 stable sort 덕분에 보장됨)
         Arrays.sort(matrix, (s1, s2) -> {
             return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
         });
