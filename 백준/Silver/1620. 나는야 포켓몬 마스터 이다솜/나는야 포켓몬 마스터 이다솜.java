4bdb85722d5e6429efd06d2f25eb205754367778
Seong57 <123578952+Seong57@users.noreply.github.com>
2025-02-25 23:13:22 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1104 ms, Memory: 121088 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -22,14 +22,29 @@ public class Main {
         }
 
         for (int i = 0; i < m; i++) {
+
             String q = br.readLine();
-            Integer key;
-            try {
-                key = Integer.parseInt(q) ;
-                System.out.println(findByInt.get(key));
-            } catch (Exception e) {
+
+            // 정규식을 이용하여 숫자인지 확인하는 방법
+            if (q.matches("\\d+")) {
+                System.out.println(findByInt.get(Integer.parseInt(q)));
+            } else {
                 System.out.println(findByName.get(q));
             }
+
+            /**  아스키코드를 이용하여 숫자인지 확인하는 방법 **/
+            // if (49 <= S.charAt(0) && S.charAt(0) <= 57) ...
+
+            /** Character 을 이용하여 숫자인지 확인하는 방법 **/
+            // Character.isDigit(q.charAt(0));
+
+            // 그냥 예외 때려잡는 방법.................은 구린듯
+//            try {
+//                key = Integer.parseInt(q) ;
+//                System.out.println(findByInt.get(key));
+//            } catch (Exception e) {
+//                System.out.println(findByName.get(q));
+//            }
         }
 
         br.close();
