d419787388b3b56526a3f78b5ff1fbdf705adc3e
Seong57 <123578952+Seong57@users.noreply.github.com>
2025-03-06 14:40:26 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(pa(n) / (pa(k) * pa(n-k)));
+
+        br.close();
+        bw.close();
+    }
+
+    public static int pa(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
