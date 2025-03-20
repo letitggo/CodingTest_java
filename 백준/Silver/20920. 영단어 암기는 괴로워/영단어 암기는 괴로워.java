f85b3e19eb78fe7f252d20948bd04dad3cf2a721
hihi <123578952+letitggo@users.noreply.github.com>
2025-03-20 23:59:18 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 700 ms, Memory: 37816 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -11,34 +11,37 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Set<String> set = new HashSet<>();
         Map<String, Integer> freqMap = new HashMap<>();
-        int maxFreq = Integer.MIN_VALUE;
-        
+
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             if (str.length() < m) continue;
             freqMap.put(str, freqMap.getOrDefault(str, 1) + 1);
-            if (freqMap.get(str) > maxFreq) maxFreq = freqMap.get(str);
-            set.add(str);
         }
 
-        ArrayList<String> list = new ArrayList<>(set);
+        ArrayList<String> list = new ArrayList<>(freqMap.keySet());
         List<String> sorted = sort(list, freqMap);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = sorted.size() - 1; i >= 0; i--) {
-            sb.append(sorted.get(i)).append("\n");
+        for (String s : sorted) {
+            sb.append(s).append("\n");
         }
+
         bw.write(sb + "");
 
         bw.flush();
     }
 
     private static List<String> sort(List<String> list, Map<String, Integer> freqMap) {
-        Comparator<String> com1 = Comparator.comparing(freqMap::get);
-        Comparator<String> com2 = Comparator.comparing(String::length);
-        list.sort(com1.thenComparing(com2).thenComparing(Comparator.reverseOrder()));
+        list.sort((o1, o2) -> {
+            if (!(freqMap.get(o1) == freqMap.get(o2))) {
+                return freqMap.get(o2) - freqMap.get(o1);
+            }
+            if (o1.length() != o2.length()) {
+                return o2.length() - o1.length();
+            }
+            return o1.compareTo(o2);
+        });
         return list;
     }
 }
