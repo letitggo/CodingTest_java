60ad2043c03d99b5b9d3b5db87a2cebd729641fc
hihi <123578952+letitggo@users.noreply.github.com>
2025-03-23 16:27:13 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static boolean[] visited;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n];
+        result = new int[m];
+        backtrack(0, 0);
+
+        bw.write(sb + "");
+        bw.flush();
+    }
+
+    private static void backtrack(int start, int depth) {
+        if (depth == m) {
+            for (int num : result) {
+                sb.append(num + 1).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = i;
+                backtrack(i, depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
+
+
+
+
