9372ed87b87339c061beb901ebc27963611d09fc
hihi <123578952+letitggo@users.noreply.github.com>
2025-04-01 15:47:33 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -7,37 +7,26 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
-
+        long[] arr = init();
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            sb.append(solution(n)).append("\n");
+            sb.append(arr[n-1]).append("\n");
         }
         bw.write(sb + "");
         bw.flush();
     }
 
 
-    private static long solution(int n) {
-        if (n > 5) {
-            long[] arr = new long[n + 1];
-            arr[0] = 1;
-            arr[1] = 1;
-            arr[2] = 1;
-            arr[3] = 2;
-            arr[4] = 2;
-
-            for (int i = 5; i < n; i++) {
-                arr[i] = arr[i - 1] + arr[i - 5];
-            }
-            return arr[n - 1];
-        } else {
-            int[] arr = new int[5];
-            arr[0] = 1;
-            arr[1] = 1;
-            arr[2] = 1;
-            arr[3] = 2;
-            arr[4] = 2;
-            return arr[n - 1];
+    private static long[] init() {
+        long[] arr = new long[100];
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 1;
+        arr[3] = 2;
+        arr[4] = 2;
+        for (int i = 5; i < 100; i++) {
+            arr[i] = arr[i - 1] + arr[i - 5];
         }
+        return arr;
     }
 }
