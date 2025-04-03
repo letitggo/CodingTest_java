eb2754c583004bc1c90637ec2acc2bf62d5406d8
hihi <123578952+letitggo@users.noreply.github.com>
2025-04-03 13:06:20 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 22488 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,64 +1,21 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int max = Integer.MIN_VALUE;
 
         int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        int[] arr = new int[n + 1];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int max = -1000;
-        int positiveSum = 0;
-        int negativeSum = 0;
-        while (st.hasMoreTokens()) {
-            int target = Integer.parseInt(st.nextToken());
-            max = Math.max(max, target);
-            if (target > 0) {
-                if (negativeSum < 0){
-                    list.add(negativeSum);
-                    negativeSum = 0;
-                }
-                positiveSum += target;
-            } else {
-                if (positiveSum > 0){
-                    list.add(positiveSum);
-                    max = Math.max(max, positiveSum);
-                    positiveSum = 0;
-                }
-                negativeSum += target;
-            }
-        }
-        if (positiveSum != 0){
-            list.add(positiveSum);
-            max = Math.max(max, positiveSum);
-        }
-        if (negativeSum != 0){
-            list.add(negativeSum);
-            max = Math.max(max, negativeSum);
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            int cur = Integer.parseInt(st.nextToken());
+            arr[i] = Math.max(arr[i - 1], 0) + cur;
+            max = Math.max(max, arr[i]);
         }
 
-        if (max < 0) {
-            System.out.println(max);
-        } else {
-            int sum = list.get(0);
-            for (int i = 1; i < list.size(); i++) {
-                if (sum + list.get(i) > 0) {
-                    sum += list.get(i);
-                } else {
-                    i++;
-                    if( i < list.size())
-                        sum = list.get(i);
-                }
-                max = Math.max(max, sum);
-            }
-            System.out.println(max);
-        }
+        System.out.println(max);
     }
 }
