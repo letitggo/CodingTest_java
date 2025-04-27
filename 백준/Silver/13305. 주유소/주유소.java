4dc0ad5b1704a9744b5afe8ce5d7610f75979ffe
hihi <123578952+letitggo@users.noreply.github.com>
2025-04-28 01:27:41 +0900

    [Silver III] Title: 주유소, Time: 408 ms, Memory: 40880 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,53 +1,28 @@
 import java.io.*;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());    // 도시의 수
+        long[] distance = Arrays.stream(br.readLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
 
-        long remain = 0;
-        int[] road = new int[n + 1];  // 도로의 길이
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n; i++) {
-            road[i] = Integer.parseInt(st.nextToken());
-            remain += road[i];
-        }
+        long[] price = Arrays.stream(br.readLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
 
-        int minIdx = 1;
-        int[] prices = new int[n + 1];  // 리터당 기름 가격
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n + 1; i++) {
-            prices[i] = Integer.parseInt(st.nextToken());
-            if (prices[i] < prices[i - 1] && i != n)
-                minIdx = i;
-        }
-        prices[n] = Integer.MAX_VALUE;
+        long totalCost = 0;
+        long minPrice = price[0];   // 첫 도시를 최소 가격으로 초기화
 
-        long ans = 0;
-        for (int i = 1; i < n; i++) {
-            if (i == minIdx) {
-                ans += (long) prices[i] * remain;
-                System.out.println(ans);
-                return;
-            } else {
-                ans += (long) prices[i] * road[i];
-                remain -= road[i];
-                int curPrice = prices[i];
-                for (int j = i + 1; j < n; j++) {
-                    if (curPrice < prices[j]) {
-                        ans += (long) curPrice * road[j];
-                        remain -= road[j];
-                        i++;
-                    } else
-                        break;
-                }
+        for (int i = 0; i < n - 1; i++) {
+            if (price[i] < minPrice) {
+                minPrice = price[i];
             }
+            totalCost += minPrice * distance[i];
         }
-        System.out.println(ans);
-
+        System.out.println(totalCost);
     }
 }
