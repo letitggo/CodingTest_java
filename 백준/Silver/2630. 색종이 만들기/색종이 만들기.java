3e9b73e4825132afd7a019d3cb3dc2beaf3a4e2c
hihi <123578952+letitggo@users.noreply.github.com>
2025-04-28 17:14:15 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 15676 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] arr;
+    static int white = 0;
+    static int blue = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        solution(0, 0, n);
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    private static void solution(int row, int col, int size) {
+        int color = arr[row][col];
+        boolean flag = false;
+
+        for (int i = row; i < size + row; i++) {
+            if (flag) break;
+            for (int j = col; j < size + col; j++) {
+                if (arr[i][j] != color){
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if (flag) {
+            solution(row, col, size / 2);
+            solution(row, col + size / 2, size / 2);
+            solution(row + size / 2, col, size / 2);
+            solution(row + size / 2, col + size / 2, size / 2);
+            return;
+        }
+
+        if (arr[row][col] == 0) {
+            white++;
+        } else blue++;
+    }
+}
