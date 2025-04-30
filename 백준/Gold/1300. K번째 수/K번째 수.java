2a0a9f5af3ccd9a45bb17431ba813d9fe3fd29a3
hihi <123578952+letitggo@users.noreply.github.com>
2025-04-30 17:29:16 +0900

    [Gold I] Title: K번째 수, Time: 136 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main {
+    static int n;
+    static long k;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+
+        System.out.println(search());
+    }
+
+    static long search() {
+        long low = 1;
+        long high = k;
+        long ans = 1;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            if (check(mid) < k) {
+                low = mid + 1;
+            } else {
+                ans = mid;
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static long check(long mid) {
+        long cnt = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            cnt += Math.min(mid / i, n);
+        }
+        return cnt;
+    }
+}
