8c5db3cc21358c0018a9550e147b5366c70d97b6
hihi <123578952+letitggo@users.noreply.github.com>
2025-05-02 20:47:05 +0900

    [Gold II] Title: 중앙값 구하기, Time: 196 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
@@ -5,50 +5,49 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        Queue<Integer> lowerHeap = new PriorityQueue<>(Collections.reverseOrder());
-        Queue<Integer> largerHeap = new PriorityQueue<>();
+
         int t = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            int length = n / 10;
-            if (n % 10 != 0)
-                length++;
             int[] arr = new int[n];
-            int idx = 0;
-            for (int j = 0; j < length; j++) {
-                st = new StringTokenizer(br.readLine());
-                while (st.hasMoreTokens()) {
-                    arr[idx] = Integer.parseInt(st.nextToken());
-                    idx++;
+
+            for (int j = 0; j <= (n - 1) / 10; j++) {
+                int[] temp = Arrays.stream(br.readLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                for (int k = 0; k < temp.length; k++) {
+                    arr[j * 10 + k] = temp[k];
                 }
             }
-            if (n % 2 == 0) {
-                sb.append(n / 2);
-            }else
-                sb.append((n / 2) + 1);
-            sb.append("\n");
+            sb.append((n + 1) / 2).append("\n");
+
+            Queue<Integer> lowerHeap = new PriorityQueue<>(Collections.reverseOrder());
+            Queue<Integer> largerHeap = new PriorityQueue<>();
+
             for (int j = 0; j < n; j++) {
-                largerHeap.offer(arr[j]);
-                if (j % 2 == 0) {
-                    for (int k = 0; k < largerHeap.size() / 2; k++) {
-                        lowerHeap.offer(largerHeap.poll());
-                    }
-                    if (j % 20 == 0 && j != 0)
-                        sb.append("\n");
-                    sb.append(largerHeap.peek()).append(" ");
-                    for (int k = 0; k < lowerHeap.size(); k++) {
-                        largerHeap.offer(lowerHeap.poll());
-                    }
+                int value = arr[j];
+                if (lowerHeap.isEmpty() || lowerHeap.peek() > value) {
+                    lowerHeap.offer(value);
+                } else
+                    largerHeap.offer(value);
+
+                if (largerHeap.size() > lowerHeap.size()) {
+                    lowerHeap.offer(largerHeap.poll());
+                } else if (lowerHeap.size() > largerHeap.size() + 1){
+                    largerHeap.offer(lowerHeap.poll());
                 }
 
+                if (j % 2 == 0) {
+                    sb.append(lowerHeap.peek()).append(" ");
+                }
+                if ((j + 1) % 20 == 0) {
+                    sb.append("\n");
+                }
             }
             sb.append("\n");
-            lowerHeap.clear();
-            largerHeap.clear();
         }
-
         System.out.println(sb);
     }
 }
