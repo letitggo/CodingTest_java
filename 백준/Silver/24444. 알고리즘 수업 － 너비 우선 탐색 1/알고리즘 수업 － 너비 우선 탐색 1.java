7adc231994793f17826191f828b748d637f400ca
hihi <123578952+letitggo@users.noreply.github.com>
2025-05-26 15:25:44 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 980 ms, Memory: 89824 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int m;
+    static int r;
+    static int[] visited;
+    static int order = 1;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static Queue<Integer> queue = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        visited = new int[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        graph.forEach(Collections::sort);
+
+        bfs(r);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < visited.length; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void bfs(int node) {
+        visited[node] = order++;
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            for (int e : graph.get(u)) {
+                if (visited[e] == 0) {
+                    visited[e] = order++;
+                    queue.add(e);
+                }
+            }
+        }
+    }
+}
